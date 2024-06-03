651dc86be881c44f7c17de0a23ca077a3112c5dc
Jasmine Tang <tanghocle456@gmail.com>
2024-06-03 10:39:19 -0700

    Time: 0 ms (100%), Space: 9.1 MB (54.62%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -3,10 +3,14 @@ private:
     int l = 0;
     std::vector<std::vector<int>> result = {};
     std::vector<int> temp = {};
+    std::map<int, int> mp = {};
 public:
     vector<vector<int>> permute(vector<int>& nums) {
        
         l = nums.size();
+        for (auto item : nums) {
+            mp[item]++;
+        }
 
         dfs_helper(nums);
 
@@ -16,15 +20,19 @@ public:
     void dfs_helper(std::vector<int> &nums) {
         if (temp.size() == l) {
             result.push_back(temp);
-        } else {
-            for (auto item : nums) {
-                // If the temp array doesn't contain the specific item, we can add it.
-                if (std::find(temp.begin(), temp.end(), item) == temp.end()) {
-                    temp.push_back(item);
-                    dfs_helper(nums);
-                    temp.pop_back();
-                }
+            return;
+        } 
+
+        for (auto &[key, value] : mp) {
+            // If the temp array doesn't contain the specific item, we can add it.
+            if (value != 0) {
+                value--;
+                temp.push_back(key);
+                dfs_helper(nums);
+                temp.pop_back();
+                value++;
             }
         }
+        
     }
 };
