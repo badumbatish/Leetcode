650c1d6dbc613e976fc8d9b41abb95014ada5bec
Jasmine Tang <tanghocle456@gmail.com>
2024-06-03 10:35:56 -0700

    Time: 2 ms (62.21%), Space: 9.4 MB (50.99%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,22 +1,28 @@
 class Solution {
-    using v_int = std::vector<int>;
+private:
+    int l = 0;
+    std::vector<std::vector<int>> result = {};
+    std::vector<int> temp = {};
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        std::vector<v_int> result = {};
-        v_int temp = {};
-        dfs_helper(temp, nums, result);
+       
+        l = nums.size();
+
+        dfs_helper(nums);
+
         return result;
     }
 
-    void dfs_helper(v_int &temp, v_int & nums, std::vector<v_int> &result) {
-        if (temp.size() == nums.size()) {
+    void dfs_helper(std::vector<int> &nums) {
+        if (temp.size() == l) {
             result.push_back(temp);
         } else {
             for (auto item : nums) {
-                if  (std::find(temp.begin(), temp.end(), item) == temp.end()) {
-                        temp.push_back(item);
-                        dfs_helper(temp, nums, result);
-                        temp.pop_back();
+                // If the temp array doesn't contain the specific item, we can add it.
+                if (std::find(temp.begin(), temp.end(), item) == temp.end()) {
+                    temp.push_back(item);
+                    dfs_helper(nums);
+                    temp.pop_back();
                 }
             }
         }
