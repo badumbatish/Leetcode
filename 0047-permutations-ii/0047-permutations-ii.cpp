6d071c4de7c1b6138ccd12c72aea9a1b4b21f2d1
Jasmine Tang <tanghocle456@gmail.com>
2024-06-03 10:46:05 -0700

    Time: 3 ms (89.16%), Space: 10.3 MB (89.8%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,43 +1,29 @@
 class Solution {
     std::vector<std::vector<int>> result;
-    std::unordered_map<int, int> mp;
     std::vector<int> temp;
-    int len = 0;
+    std::map<int, int> mp;
+    int length = 0;
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        len = nums.size();
         for (auto i : nums) mp[i]++;
+        length = nums.size();
         recursive();
 
         return result;
     }
 
     void recursive() {
-        if (temp.size() == len) {
+        if (temp.size() == length) {
             result.push_back(temp);
             return;
         }
-        for (auto it = mp.begin(); it != mp.end(); it++) {
-            char ch = it->first;
 
-            // the value == 0 means we ran out of this choice,
-            // we really should pick another option in the map
-            if (it->second == 0) continue;
-            
-            // Use the option
-            temp.push_back(ch);
+        for (auto &[key, cnt] : mp) {
+            if (cnt == 0) continue;
 
-            // Mark that we use it
-            it->second--;
-
-            // Go to the next empty place where we haven't use the option
+            temp.push_back(key); cnt--;
             recursive();
-
-            // Un-use the option
-            temp.pop_back();
-
-            // Mark that we un-use it.
-            it->second++;
+            temp.pop_back(); cnt++;
         }
     }
 };
