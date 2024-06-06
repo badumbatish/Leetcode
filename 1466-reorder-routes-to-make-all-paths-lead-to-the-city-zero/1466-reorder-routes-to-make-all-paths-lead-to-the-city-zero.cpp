f897768b7eb9b4fd7f0d13381683acd6e33e226e
Jasmine Tang <tanghocle456@gmail.com>
2024-06-06 10:36:17 -0500

    Time: 219 ms (94.1%), Space: 104.8 MB (83.31%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -0,0 +1,33 @@
+class Solution {
+private:
+    int n;
+    std::vector<std::vector<std::pair<int, int>>> adj;
+
+    int count = 0;
+public:
+    int minReorder(int n, vector<vector<int>>& connections) {
+        this->n = n;
+        adj = std::vector<std::vector<std::pair<int, int>>>(n);
+        for (auto &connection : connections) {
+            
+            // from connnections, we know that con[0] -> con[1] is there.
+            // so we need to add con[0] <- con[1]
+            adj[connection[0]].push_back({connection[1], 1}); // this is from con[0] pov
+            adj[connection[1]].push_back({connection[0], 0}); // this is from con[1] pov
+
+        }
+
+        dfs(0, -1);
+
+        return count;
+    }
+
+    void dfs(int node, int parent) {
+        for (auto &[neighbor, sign] : adj[node]) {
+            if (neighbor == parent) continue;
+
+            count += sign;
+            dfs(neighbor, node);
+        }
+    }
+};
