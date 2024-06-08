8b0b5e32c41b0ebf8903dde1cb06ccd9875566c2
Jasmine Tang <tanghocle456@gmail.com>
2024-06-07 20:12:55 -0500

    Time: 82 ms (92.31%), Space: 34.2 MB (73.21%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -1,49 +1,35 @@
 class Solution {
 public:
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
-        int rows = maze.size(),  cols = maze[0].size();
+        int rows = maze.size();
+        int cols = maze[0].size();
 
-        std::vector<std::pair<int, int>> dirs = {{1,0}, {-1, 0}, {0, 1}, {0, -1}};
+        auto delta_dir = std::vector<std::pair<int, int>> { {0, 1}, {0 ,-1}, {1, 0}, {-1, 0}};
 
-        // Mark the entrance as visited since its not a exit.
         int start_row = entrance[0], start_col = entrance[1];
-
         maze[start_row][start_col] = '+';
+        // row, column, and distance
+        std::queue<std::array<int, 3>> q;
 
-        // Start BFS from the entrance, and use a queue `queue` to store all 
-        // the cells to be visited.
-        std::queue<std::array<int, 3>> queue;
-        queue.push({start_row, start_col, 0});
+        q.push({start_row, start_col, 0});
 
-        while (!queue.empty()) {
-            auto [cur_row, cur_col, cur_distance] = queue.front();
+        while (!q.empty()) {
+            auto [cur_row, cur_col, dist] = q.front(); q.pop();
 
-            queue.pop();
+            for (auto [dx, dy] : delta_dir) {
+                int row = cur_row + dx;
+                int col = cur_col + dy;
 
-            // For the current cell, check its four neighbor cells.
+                if (0 <= row && row < rows && 0 <= col && col < cols && maze[row][col] == '.') {
 
-            for (auto [row_dis, col_dis] : dirs) {
-                int next_row = cur_row + row_dis;
-                int next_col = cur_col + col_dis;
+                    if (row == 0 || row == rows - 1 || col == 0 || col == cols - 1) return dist + 1;
 
-                // If there exists an unvisited empty neighbor:
-                if (0 <= next_row && next_row < rows && 0 <= next_col && next_col < cols && 
-                    maze[next_row][next_col] == '.') {
-
-                        // If this empty cell is an exit, return distance + 1.
-
-                        if (next_row == 0 || next_row == rows - 1 || next_col == 0 || 
-                        next_col == cols - 1) {
-                            return cur_distance + 1;
-                        }
-
-                        // Otherwise, add this cell to 'queue' and mark it as visited.
-                        maze[next_row][next_col] = '+';
-                        queue.push({next_row, next_col, cur_distance + 1});
+                    maze[row][col] = '+';
+                    q.push({row, col, dist + 1});
                 }
             }
-
         }
+
         return -1;
     }
 };
