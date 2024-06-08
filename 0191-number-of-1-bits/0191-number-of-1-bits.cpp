0f02f53f4d922e719afc2f0dbc7058f4fb903b4b
Jasmine Tang <tanghocle456@gmail.com>
2024-06-08 13:58:19 -0500

    Time: 4 ms (30.25%), Space: 7.4 MB (23.67%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int count = 0;
+
+        while (n) {
+            count += (n % 2);
+            n /= 2;
+        }
+
+        return count;
+
+    }
+};
