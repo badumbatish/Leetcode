29338f604b9b4cd268cedce32de82f9944543818
Jasmine Tang <tanghocle456@gmail.com>
2024-06-09 21:32:15 -0500

    Time: 247 ms (54.24%), Space: 93 MB (98.7%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -4,11 +4,11 @@ public:
         int ans = -1;
 
         if (intervals.size() == 0) return 0;
-        std::sort(intervals.begin(), intervals.end(), [](auto a, auto b){ return a[1] < b[1]; });
+        std::sort(intervals.begin(), intervals.end(), [](auto& a, auto& b){ return a[1] < b[1]; });
 
-        std::vector<int> prev = intervals[0];
+        auto &prev = intervals[0];
 
-        for (auto i : intervals) {
+        for (auto& i : intervals) {
             if (prev[1] > i[0]) ans++;
             else prev = i;
         }
