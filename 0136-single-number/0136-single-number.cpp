60a6d57d438eddb053da39c42d5e4ee5cee1686e
Jasmine Tang <tanghocle456@gmail.com>
2024-06-12 14:35:22 -0500

    Time: 11 ms (81.69%), Space: 19.1 MB (84.8%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,6 +1,14 @@
+#if PARALLEL
+#include <execution>
+#define SEQ std::execution::seq,
+#define PAR std::execution::par,
+#else
+#define SEQ
+#define PAR
+#endif
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        return std::accumulate(std::begin(nums), std::end(nums), 0, [](int a, int b) { return a ^ b; });
+        return std::reduce(PAR nums.begin(), nums.end(), 0, std::bit_xor<int>());
     }
 };
