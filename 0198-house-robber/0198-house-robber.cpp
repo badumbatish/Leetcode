8d6508674a72e16ae6862d6c58342d9742786c73
Jasmine Tang <tanghocle456@gmail.com>
2024-06-24 10:21:55 -0500

    Time: 0 ms (100%), Space: 8.9 MB (94.46%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -10,8 +10,16 @@ public:
         int c2 = nums[0];
 
         for (int i = 2; i < nums.size(); i++) {
+            // The amount that the thief can get is 
+            // The maximum between the 1-away, or 
+                // the previous 2-away one, coupled with the current one.
             amount = max(c2 + nums[i], c1);
+
+            // The two-away is the max between the previous two-away and the 1-away
+            // (since the house robber can elect to be picky)
             c2 = max(c2, c1);
+
+            // The 1-away is always the amount
             c1 = amount;
         }
 
