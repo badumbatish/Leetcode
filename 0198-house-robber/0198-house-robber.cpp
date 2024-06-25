07af6e3771de29511a17575385b3f88680c1e69d
Jasmine Tang <tanghocle456@gmail.com>
2024-06-25 16:52:33 -0500

    Time: 0 ms (100%), Space: 9 MB (94.43%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        if (nums.size() == 1) return nums[0];
-        if (nums.size() == 2) return max(nums[0], nums[1]);
+        if (nums.size() <= 2) return *std::ranges::max_element(nums);
 
-        auto amount = nums[1];
+        auto two_away = nums[0];
+        auto one_away = nums[1];
 
-        auto c1 = nums[1];
-        auto c2 = nums[0];
+        auto amount = max(two_away, one_away);
+        for(size_t i = 2; i < nums.size(); i++) {
+            amount = max(two_away + nums[i], one_away);
 
-        for (size_t i = 2; i < nums.size(); i++) {
-            // The amount that the thief can get is 
-            // The maximum between the 1-away, or 
-                // the previous 2-away one, coupled with the current one.
-            amount = max(c2 + nums[i], c1);
-
-            // The two-away is the max between the previous two-away and the 1-away
-            // (since the house robber can elect to be picky)
-            c2 = max(c2, c1);
-
-            // The 1-away is always the amount
-            c1 = amount;
+            two_away = max(two_away, one_away);
+            one_away = amount;
         }
-
+        
         return amount;
     }
 };
