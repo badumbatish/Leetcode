ccb9c926ba23161382498375ff0b9ad2307a1185
Jasmine Tang <tanghocle456@gmail.com>
2024-06-27 08:07:15 -0500

    Time: 143 ms (45.45%), Space: 70.7 MB (92.91%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -1,25 +1,6 @@
 class Solution {
 public:
-    int findCenter(vector<vector<int>>& edges) {
-        std::unordered_map<int, int> ump;
-
-        for (auto& edge : edges) {
-            ump[edge[0]]++;
-            ump[edge[1]]++;
-
-    
-        }
-
-        int node = 0;
-        int max = 0;
-
-        for (auto & [n, degree] : ump) {
-            if (max < degree ) {
-                max = degree; 
-                node = n;
-            }
-         }
-
-        return node;
+    int findCenter(vector<vector<int>>& e) {
+        return e[0][0] == e[1][0] || e[0][0] == e[1][1] ? e[0][0] : e[0][1];
     }
 };
