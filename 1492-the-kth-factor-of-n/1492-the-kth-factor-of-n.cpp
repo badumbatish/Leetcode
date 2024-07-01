429103d9b7a8e5a1ad88c63c5e401c2e9ec4a281
Jasmine Tang <tanghocle456@gmail.com>
2024-06-30 21:36:24 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
@@ -3,17 +3,25 @@ public:
     int kthFactor(int n, int k) {
         int count = 0;
 
-        int factor = 0;
-        for (int i = 1; i <= n; i++) {
-            if ( n % i == 0) {
-                count++;
-                if (count == k) factor = i;
-            }
-            
+        for (int i = 1; i<= n; i++) {
+            if ( n % i == 0 && n / i == i) count++;  
+            else if (n % i == 0) count += 2;    
         }
 
         if (count < k) return -1;
-        return factor;
-        
+
+        int c = 0;
+        for (int i = 1; i*i <= n; i++) {
+            if (n % i == 0) {
+                c++;
+
+                if (c == k || count + 1 == k + c) {
+                    if (k > count / 2) return n/i;
+                    return i;
+                }
+            }
+        }      
+
+        return -1;
     }
 };
