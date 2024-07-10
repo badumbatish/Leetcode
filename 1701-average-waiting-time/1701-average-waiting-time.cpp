441b0012a14b50951aa6693ceae31c41874d40c3
Jasmine Tang <tanghocle456@gmail.com>
2024-07-09 19:00:53 -0500

    Time: 161 ms (36.15%), Space: 76.3 MB (95.63%) - LeetHub

--- 1701-average-waiting-time/1701-average-waiting-time.cpp
+++ 1701-average-waiting-time/1701-average-waiting-time.cpp
@@ -2,20 +2,13 @@ class Solution {
 public:
     double averageWaitingTime(vector<vector<int>>& customers) {
         int64_t sum = 0;
+        int32_t finish_time = 0;
 
-        int finish_time = 0;
-
-        for (auto &customer : customers) {
+        for(auto &customer : customers) {
             finish_time = max(finish_time, customer[0]) + customer[1];
-
-            sum += finish_time - customer[0];
-            
-
-            std::cout << sum << " " << finish_time << std::endl;
-            
+            sum += (finish_time) - customer[0];
         }
 
-        
         return sum * 1.0 / customers.size();
     }
 };
