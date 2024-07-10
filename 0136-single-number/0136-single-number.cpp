28880ce28027105481a44a812f5d3965d6694a60
Jasmine Tang <tanghocle456@gmail.com>
2024-07-09 21:57:46 -0500

    Time: 9 ms (88.01%), Space: 19.3 MB (57.22%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,14 +1,12 @@
-#if PARALLEL
-#include <execution>
-#define SEQ std::execution::seq,
-#define PAR std::execution::par,
-#else
-#define SEQ
-#define PAR
-#endif
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        return std::reduce(PAR nums.begin(), nums.end(), 0, std::bit_xor<int>());
+    //         If we take XOR of zero and some bit, it will return that bit
+    // a⊕0 = a
+    // If we take XOR of two same bits, it will return 0
+    // a⊕a = 0
+    // a⊕b⊕a =(a⊕a)⊕b = 0⊕b = b
+    // So we can XOR all bits together to find the unique number.
+        return std::reduce(nums.begin(), nums.end(), 0, std::bit_xor<int>());
     }
 };
