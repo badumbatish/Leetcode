200eb79339a5cdac1e650b47d1c9af651c9150a3
Jasmine Tang <tanghocle456@gmail.com>
2024-07-11 13:45:15 -0500

    Time: 27 ms (5.95%), Space: 18.5 MB (18.47%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -1,35 +1,30 @@
 class Solution {
-public:
-void dfs(std::vector<int> adj_list[], std::unordered_set<int> &u_set, int src) {
-    u_set.insert(src);
+    void dfs(std::vector<std::vector<int>> &adj_list, std::set<int>& visited, int source) {
+        visited.insert(source);
 
-    for (int i = 0; i < adj_list[src].size(); i++)  {
-        if (u_set.count(adj_list[src][i]) == 0) {
-            dfs(adj_list, u_set, adj_list[src][i]);
-        } 
+        for (int i = 0; i < adj_list[source].size(); i++) {
+            if (visited.count(adj_list[source][i]) == 0) {
+                dfs(adj_list, visited, adj_list[source][i]);
+            }
+        }
     }
-}
+public:
     int countComponents(int n, vector<vector<int>>& edges) {
-        if (n == 0) return 0;
-
-        int components = 0;
-        std::unordered_set<int> u_set;
-
-        std::vector<int> adj_list[n];
-
-        for (auto &edge : edges) {
-            adj_list[edge[0]].push_back(edge[1]);
+        std::vector<std::vector<int>> adj_list(n);
+        std::set<int> visited;
+        for(auto& edge : edges) {
             adj_list[edge[1]].push_back(edge[0]);
+            adj_list[edge[0]].push_back(edge[1]);
         }
 
+        int components = 0;
         for (int i = 0; i < n; i++) {
-            if (u_set.count(i) == 0) {
+            if (visited.count(i) == 0) {
                 components++;
-                dfs(adj_list, u_set, i);
+                dfs(adj_list, visited, i);
             }
         }
 
         return components;
-
     }
 };
