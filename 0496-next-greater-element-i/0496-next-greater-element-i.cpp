ff10de314a4204eea7d5b6e4b2004005aad29463
Jasmine Tang <tanghocle456@gmail.com>
2024-07-11 19:06:51 -0500

    Time: 8 ms (31.75%), Space: 12.1 MB (27.58%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        for (auto &num : nums1) {
-            int index = std::ranges::find(nums2, num) - nums2.begin();
+        std::stack<int> stck;
+        std::map<int, int> map_num_to_greater;
 
-            bool minus_one = true;
-            for (int i = index + 1; i < nums2.size(); i++) {
-                if (nums2[i] > nums2[index]) {
-                    num = nums2[i];
-                    minus_one = false;
-                    break;
-                }
+        for (auto num : nums2) {
+            while (!stck.empty() && num > stck.top()) {
+                map_num_to_greater[stck.top()] = num;
+                stck.pop();
             }
-            if (minus_one) num = -1;
+
+            stck.push(num);
         }
 
+        while (!stck.empty()) {
+            map_num_to_greater[stck.top()] = -1;
+            stck.pop();
+        }
+        for (auto &num : nums1){ 
+            num = map_num_to_greater[num];
+        }
 
         return nums1;
     }
