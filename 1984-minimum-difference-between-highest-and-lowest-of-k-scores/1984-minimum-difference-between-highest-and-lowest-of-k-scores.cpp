bb56d12ae2b15e4b499c64986e016ff3e0a077b0
Jasmine Tang <tanghocle456@gmail.com>
2024-07-13 13:13:18 -0500

    Time: 7 ms (91.48%), Space: 16.4 MB (58.18%) - LeetHub

--- 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.cpp
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.cpp
@@ -6,8 +6,8 @@ public:
         std::ranges::sort(nums);
 
         int diff = INT_MAX;
-        for (int i = 1; i < nums.size() ;i ++) {
-            diff = std::min(nums[i]- nums[i-1], diff);
+        for (int i = k - 1; i < nums.size() ;i ++) {
+            diff = std::min(nums[i]- nums[i - k + 1], diff);
         }
         
         return diff;
