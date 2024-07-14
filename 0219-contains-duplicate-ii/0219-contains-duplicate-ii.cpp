0881dc239b9dbc390c187cc3c828bec7190d4a83
Jasmine Tang <tanghocle456@gmail.com>
2024-07-14 12:40:43 -0500

    Time: 865 ms (8.01%), Space: 62.9 MB (92.08%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = max(i - k, 0); j < i; j++) {
+                if (nums[i] == nums[j]) return true;
+            }
+        }
+
+        return false;
+    }
+};
