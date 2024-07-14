0b366b609284becfd71cb0ea00b46233a0f4e162
Jasmine Tang <tanghocle456@gmail.com>
2024-07-14 12:58:47 -0500

    Time: 1592 ms (5%), Space: 98.4 MB (5.65%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -2,14 +2,29 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         if (s1.size() > s2.size()) return false;
-        std::ranges::sort(s1);
-        for (int i = 0; i < s2.size() - s1.size() + 1; i++) {
-            auto sb = s2.substr(i, s1.size());
-            std::ranges::sort(sb);
+        
+        std::map<char, int> map_1;
+        for (auto ch : s1) map_1[ch]++;
 
-            if (sb == s1) return true;
+        for (int i = 0; i < s2.size() - s1.size() + 1; i++) {
+            std::map<char, int> map_2;
+
+            for (int j = 0; j < s1.size(); j++) {
+                map_2[s2[i+j]]++;
+            }
+            if (matches(map_1, map_2)) return true;
         }
 
         return false;
     }
+
+    bool matches(std::map<char, int> &a, std::map<char, int>&b) {
+        for (auto [key, value] : a) {
+            if (a[key] - b[key] != 0) return false;
+        }
+
+        return true;
+    }
+
+
 };
