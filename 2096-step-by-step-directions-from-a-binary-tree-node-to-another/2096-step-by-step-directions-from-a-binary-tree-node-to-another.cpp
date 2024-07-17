3ce6c77d36c44e1cd745d4749166962739c74ad8
Jasmine Tang <tanghocle456@gmail.com>
2024-07-16 19:53:38 -0500

    Time: 790 ms (16.18%), Space: 232.2 MB (7.63%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.cpp
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.cpp
@@ -1,103 +1,104 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
+    std::map<int, TreeNode*> parent_map;
+    std::queue<TreeNode*> q;
+    std::set<TreeNode*> visited;
+    std::unordered_map<TreeNode*, std::pair<TreeNode*, string>> path_tracker;
+
 public:
     string getDirections(TreeNode* root, int startValue, int destValue) {
-        // Map to store parent nodes
-        unordered_map<int, TreeNode*> parentMap;
+        
+    
 
-        // Find the start node and populate parent map
-        TreeNode* startNode = findStartNode(root, startValue);
-        populateParentMap(root, parentMap);
+        auto start_node = find_node(root, startValue);
+        populate_parent_map(root);
 
-        // Perform BFS to find the path
-        queue<TreeNode*> q;
-        q.push(startNode);
-        unordered_set<TreeNode*> visitedNodes;
-        // Key: next node, Value: <current node, direction>
-        unordered_map<TreeNode*, pair<TreeNode*, string>> pathTracker;
-        visitedNodes.insert(startNode);
 
-        while (!q.empty()) {
-            TreeNode* currentNode = q.front();
-            q.pop();
+        q.push(start_node);
+        visited.insert(start_node);
 
-            // If destination is reached, return the path
-            if (currentNode->val == destValue) {
-                return backtrackPath(currentNode, pathTracker);
+        while(!q.empty()) {
+            TreeNode* current_node = q.front(); q.pop();
+            if (current_node->val == destValue) {
+                return back_track(current_node);
             }
 
-            // Check and add parent node
-            if (parentMap.find(currentNode->val) != parentMap.end()) {
-                TreeNode* parentNode = parentMap[currentNode->val];
-                if (visitedNodes.find(parentNode) == visitedNodes.end()) {
-                    q.push(parentNode);
-                    pathTracker[parentNode] = {currentNode, "U"};
-                    visitedNodes.insert(parentNode);
+
+            // Work up to the root
+            if (parent_map.find(current_node->val) != parent_map.end()) {
+                TreeNode* parent_node = parent_map[current_node->val];
+
+                // If we haven't visited the parent,
+                // We say that the path from the current node to parent node is UP 
+                // And we visit the parent node next.
+                if (visited.count(parent_node) == 0) {
+                    q.push(parent_node);
+                    path_tracker[parent_node] = {current_node, "U"};
+                    visited.insert(parent_node);
                 }
             }
 
-            // Check and add left child
-            if (currentNode->left != nullptr &&
-                visitedNodes.find(currentNode->left) == visitedNodes.end()) {
-                q.push(currentNode->left);
-                pathTracker[currentNode->left] = {currentNode, "L"};
-                visitedNodes.insert(currentNode->left);
+            // Work down to the left
+            // If we haven't visited the left
+            // We say that the path from the current node to the left is LEFT
+            if (current_node->left != nullptr && visited.count(current_node->left) == 0) {
+                q.push(current_node->left);
+                path_tracker[current_node->left] = {current_node, "L"};
+                visited.insert(current_node->left);
             }
 
-            // Check and add right child
-            if (currentNode->right != nullptr &&
-                visitedNodes.find(currentNode->right) == visitedNodes.end()) {
-                q.push(currentNode->right);
-                pathTracker[currentNode->right] = {currentNode, "R"};
-                visitedNodes.insert(currentNode->right);
+            // Work down to the right
+
+            if (current_node->right != nullptr && visited.count(current_node->right) == 0) {
+                q.push(current_node->right);
+                path_tracker[current_node->right] = {current_node, "R"};
+                visited.insert(current_node->right);
             }
         }
-
-        // This line should never be reached if the tree is valid
+        
         return "";
     }
 
-private:
-    string backtrackPath(
-        TreeNode* node,
-        unordered_map<TreeNode*, pair<TreeNode*, string>> pathTracker) {
-        string path;
-        // Construct the path
-        while (pathTracker.count(node)) {
-            // Add the directions in reverse order and
-            // move on to the previous node
-            path += pathTracker[node].second;
-            node = pathTracker[node].first;
+    TreeNode* find_node(TreeNode* root, int find) {
+        if (root == nullptr) return nullptr;
+        if (root->val == find) return root;
+        if (auto l = find_node(root->left, find)) return l;
+        if (auto r = find_node(root->right, find)) return r;
+
+        return nullptr;
+    }
+
+    void populate_parent_map(TreeNode* root) {
+        if (root == nullptr) return;
+        if (root->left != nullptr) {
+            parent_map[root->left->val] = root;
+            populate_parent_map(root->left);
         }
-        reverse(path.begin(), path.end());
+        if (root->right != nullptr) {
+            parent_map[root->right->val] = root;
+            populate_parent_map(root->right);
+        }
+    }
+    
+    std::string back_track(TreeNode* root) {
+        std::string path;
+
+        while (path_tracker.count(root)) {
+            path += path_tracker[root].second;
+            root = path_tracker[root].first;
+        }
+
+        std::reverse(path.begin(), path.end());
         return path;
-    }
-
-    void populateParentMap(TreeNode* node,
-                           unordered_map<int, TreeNode*>& parentMap) {
-        if (node == nullptr) return;
-
-        // Add children to the map and recurse further
-        if (node->left != nullptr) {
-            parentMap[node->left->val] = node;
-            populateParentMap(node->left, parentMap);
-        }
-
-        if (node->right != nullptr) {
-            parentMap[node->right->val] = node;
-            populateParentMap(node->right, parentMap);
-        }
-    }
-
-    TreeNode* findStartNode(TreeNode* node, int startValue) {
-        if (node == nullptr) return nullptr;
-
-        if (node->val == startValue) return node;
-
-        TreeNode* leftResult = findStartNode(node->left, startValue);
-
-        // If left subtree returns a node, it must be StartNode. Return it
-        // Otherwise, return whatever is returned by right subtree.
-        if (leftResult != nullptr) return leftResult;
-        return findStartNode(node->right, startValue);
     }
 };
