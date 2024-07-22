70a2100e7db84bca7b71612103f271a75c31226c
Jasmine Tang <tanghocle456@gmail.com>
2024-07-21 23:37:19 -0500

    Time: 33 ms (40.77%), Space: 25.3 MB (74.29%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.cpp
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
 vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
-    std::vector<std::pair<int, string>> people(names.size());
-    std::ranges::transform(heights, names, people.begin(), 
-                      [](int h, string& n) { return std::pair{h, n}; });
+    std::vector<std::pair<int, std::string>> people(names.size());
+    std::ranges::transform(heights, names, people.begin(),
+                    [](int h, std::string&n) { return std::pair(h, n); });
     std::ranges::sort(people, std::greater());
-    std::ranges::transform(people, names.begin(), 
-                      [](const auto& p) { return p.second; });
+    std::ranges::transform(people, names.begin(),
+                    [](const auto& p) { return p.second; });
     return names;
 }
 };
