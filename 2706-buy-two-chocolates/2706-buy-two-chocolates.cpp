6b064a24954b4dd53e4d506e3ea2c72adaa93ed1
Jasmine Tang <tanghocle456@gmail.com>
2024-07-25 14:58:01 -0500

    Time: 20 ms (12.58%), Space: 51.8 MB (15.4%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -4,7 +4,7 @@ public:
         std::nth_element(prices.begin(), prices.begin() + 2, prices.end());
 
         int buy = prices[0] + prices[1];
-        std::cout << buy << std::endl;
+        
         if (buy > money) return money;
         return money - buy;
     }
