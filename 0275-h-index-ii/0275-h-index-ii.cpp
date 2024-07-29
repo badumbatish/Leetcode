dcc85e482a48b725265ae1017194a8ca601f9060
Jasmine Tang <tanghocle456@gmail.com>
2024-07-29 18:37:39 -0500

    Time: 14 ms (45.94%), Space: 21.2 MB (28.71%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.cpp
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        if (citations.size() == 1) {
-            if (citations.front() == 0) return 0;
-            return 1;
-        }
+
         int64_t left = 0, right = citations.size();
 
         while (left < right) {
             int64_t mid = left + (right - left) / 2;
-            printf("%d\n", mid);
-            if (mid + 1 >= citations[mid]) {
-                left = mid + 1;
-            } else right = mid;
+            if (citations[mid] >= citations.size() - mid) right = mid;
+            else left = mid + 1;
         }
 
-        return left;
+        return citations.size() - left;
     }
 };
