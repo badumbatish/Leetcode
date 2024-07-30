36bb9caded4b2a462f186ad8ca09da3e8e7b6493
Jasmine Tang <tanghocle456@gmail.com>
2024-07-29 19:51:58 -0500

    Time: 75 ms (91.23%), Space: 23.7 MB (73.37%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -3,15 +3,14 @@ public:
     int minimumDeletions(string s) {
         std::stack<int> stck;
         int mini = 0;
+        int b_count = 0;
         for(int i = 0; i < s.size(); i++) {
-            if (!stck.empty() && stck.top() > s[i]) {
-                std::cout << i << std::endl;
-                stck.pop();
+            if (s[i] == 'a' && b_count > 0) {
                 mini++;
+                b_count--;
             }
-            if (s[i] == 'b') stck.push(s[i]);
+            if (s[i] == 'b') b_count++;
         }
-
         return mini;
     }
 };
