8bf5ed3d8ef3f4462bf2bb751523ddd90e563c58
Jasmine Tang <tanghocle456@gmail.com>
2024-08-01 10:39:05 -0500

    Time: 0 ms (100%), Space: 7.9 MB (35.66%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        int m = word1.size();
-        int n = word2.size();
+        int i = 0, j = 0, m = word1.size(), n = word2.size();
+        std::string res;
 
-        std::string result = "";
-        int i = 0, j = 0;
-        
-        while (i < m || j < n) 
-        {
-            if (i < m) result += word1[i++];
-            if (j < n) result += word2[j++];
-        }
-
-        return result;
+        while (i < m || j < n) {
+            if (i < m) res.push_back(word1[i++]);
+            if (j < n) res.push_back(word2[j++]);
+        }   
+        return res;
     }
 };
