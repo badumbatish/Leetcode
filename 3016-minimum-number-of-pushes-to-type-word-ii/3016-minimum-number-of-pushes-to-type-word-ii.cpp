90cc15d47223356844b982232c908ffe699d0530
Jasmine Tang <tanghocle456@gmail.com>
2024-08-05 21:32:57 -0500

    Time: 188 ms (8.1%), Space: 32.2 MB (5.55%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        std::map<char, int> freq_char;
+        for (auto ch : word) freq_char[ch]++;
+
+        std::vector<std::pair<int, char>> sorted_freq;
+        for (auto [ch, freq] : freq_char) sorted_freq.push_back({freq, ch});
+        std::sort(sorted_freq.rbegin(), sorted_freq.rend());
+
+        std::map<char, int> assigned_char_weight;
+        int cycle = 8;
+        int weight = 1;
+        for (auto [freq, ch] : sorted_freq) {
+            assigned_char_weight[ch] = weight;
+            cycle--;
+            if (cycle == 0) {
+                weight++;
+                cycle = 8;
+            }
+        }
+        int sum = 0;
+
+        for (auto [ch, freq] : freq_char) {
+            sum += assigned_char_weight[ch] * freq;
+        }
+       
+        return sum;
+    }
+};
