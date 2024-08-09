2b3f7ac14c2d6bb3ca75ae0e11b44360664563e2
Jasmine Tang <tanghocle456@gmail.com>
2024-08-08 22:28:48 -0500

    Time: 15 ms (84.5%), Space: 24.9 MB (39.01%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -6,18 +6,11 @@ public:
 
         while (lo < hi) {
             int mid = lo + (hi - lo) / 2;
-            bool havles_are_even = (hi - mid) % 2 == 0;
-
-            if (nums[mid + 1] == nums[mid]) {
-                if (havles_are_even) lo = mid + 2;
-                else hi = mid - 1;
-            } else if (nums[mid - 1] == nums[mid]) {
-                if (havles_are_even) hi = mid - 2;
-                else lo = mid + 1;
-            } else return nums[mid];
+            if (mid % 2 == 1) mid--;
+            if (nums[mid] == nums[mid+1]) lo = mid + 2;
+            else hi = mid;
         }
+
         return nums[lo];
-
     }
-
 };
