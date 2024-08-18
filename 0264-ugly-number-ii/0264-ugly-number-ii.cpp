ddc4d5a3f04946148c1c1d9432b14083440dd2f0
Jasmine Tang <tanghocle456@gmail.com>
2024-08-18 01:09:19 -0700

    Time: 207 ms (5.05%), Space: 42.7 MB (5.1%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -4,28 +4,22 @@ public:
         std::vector<int64_t> dp = {1};
         std::priority_queue<int64_t, std::vector<int64_t>, std::greater<int64_t>> pq;
         std::set<int64_t> considered = {1};
+
+        std::vector<int64_t> multiplier = {2, 3, 5};
         pq.push(1);
         for (int i = 1; i <= n; i++)  {
             auto candidate = pq.top(); pq.pop();
             std::cout << candidate << std::endl;
             dp.push_back(candidate);
-            considered.insert(candidate);
-        
-            if (!considered.count(candidate*2)) {
-                pq.push(candidate * 2);
-                considered.insert(candidate * 2);
+
+            for (auto mult : multiplier) {
+                if (!considered.count(candidate*mult)) {
+                    pq.push(candidate * mult);
+                    considered.insert(candidate * mult);
+                }
             }
             
-            if (!considered.count(candidate*3)) {
-                pq.push(candidate * 3);
-                considered.insert(candidate * 3);
-
-            }
-            if (!considered.count(candidate*5)) {
-                pq.push(candidate * 5);
-                considered.insert(candidate * 5);
-
-            }
+    
 
         }
 
