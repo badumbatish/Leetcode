d6fb70df1c06a709bd322f41a6d12374e68d74dd
Jasmine Tang <tanghocle456@gmail.com>
2024-08-26 23:34:54 -0700

    Time: 91 ms (99.06%), Space: 53.3 MB (98.63%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,30 +1,28 @@
 class Solution {
-    using NODE = int;
-    using DISTANCE = double;
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
-        std::unordered_map<int, std::set<std::pair<NODE, DISTANCE>>> adj_list;
-        for (int i = 0; i < edges.size(); i++) {
-            adj_list[edges[i][0]].insert({edges[i][1], succProb[i]});
-            adj_list[edges[i][1]].insert({edges[i][0], succProb[i]});
-        }
-    
-        std::unordered_map<NODE, DISTANCE> dist_map;
-        std::priority_queue<std::pair<DISTANCE, NODE>> q;
-        q.push({1.0, start_node});
-        dist_map[start_node] = 1.0;
+        std::vector<double> max_prob(n, 0);
+        max_prob[start_node] = 1.0;
 
-        while (!q.empty()) {
-            auto [dist, from] = q.top(); q.pop();
-            for (auto &[to, dist_from_to] : adj_list[from]) {
-                auto connected_dist = dist * dist_from_to;
-                if (!dist_map.contains(to) || connected_dist > dist_map[to]) {
-                    q.push({connected_dist, to});
-                    dist_map[to] = connected_dist;
+        for (int i = 0; i < n - 1; i++) {
+            auto has_update = false;
+            for (int j = 0; j < edges.size(); j++) {
+                int u = edges[j][0], v = edges[j][1];
+                double path_prob = succProb[j];
+
+                if (max_prob[u] * path_prob > max_prob[v]) {
+                    max_prob[v] = max_prob[u] * path_prob;
+                    has_update = true;
+                }
+                if (max_prob[v] * path_prob > max_prob[u]) {
+                    max_prob[u] = max_prob[v] * path_prob;
+                    has_update = true;
                 }
             }
+
+            if (!has_update) break;
         }
 
-        return dist_map[end_node];
+        return max_prob[end_node];
     }
 };
