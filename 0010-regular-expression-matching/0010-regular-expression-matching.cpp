6cfc70e40a81edaeb7cf589c121d56c7ba7a18cc
Jasmine Tang <tanghocle456@gmail.com>
2024-10-04 21:28:26 -0700

    Time: 3 ms (70.32%), Space: 9 MB (56.08%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,60 +1,34 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        int i = 0, j = 0;
+        // Memoization table to store results for each (i, j) pair
+        vector<vector<int>> memo(s.size() + 1, vector<int>(p.size() + 1, -1));
+        return matchHelper(s, p, 0, 0, memo);
+    }
 
-        for (j = 0; j < p.size(); j++) {
-            if (p[j] != '.' && p[j] != '*') {
-                if (p[j] == s[i]) {
-                    i++;
-                    continue;
-                } else if (j + 1< p.size() && p[j+1] == '*'){
-                    continue; 
-                } else {
-                    return false;
-                }
-            } else if (p[j] == '.') {
-                if (j+1 < p.size() && p[j+1] != '.' && p[j+1] != '*') {
-                    i++;
-                    continue;
-                }
-
-                if (j+1 < p.size() && p[j+1] == '.') {
-                    i++;
-                    continue;
-                }
-
-                if (j+1 < p.size() && p[j+1] == '*') {
-                    continue;
-                }
-
-                i++;
-            } else if (p[j] == '*') {
-                if (p[j-1] == '.') {
-                    if (j + 1 < p.size()) {
-                        while (i < s.size() && s[i] != p[j+1]) {
-                            i++;
-                        }
-                    } else {
-                        return true;
-                    }
-                } else {
-                    if (j + 1 < p.size() && p[j-1] == p[j+1]) {
-                        while (i+1 < s.size() && s[i] == p[j-1]) {
-                            i++;
-                        }
-                    } else {
-                        while (i < s.size() && s[i] == p[j-1]) {
-                            i++;
-                        }
-                    }
-
-                }
-            }
+private:
+    bool matchHelper(const string &s, const string &p, int i, int j, vector<vector<int>> &memo) {
+        // Check if we've already computed this subproblem
+        if (memo[i][j] != -1) return memo[i][j];
+        
+        // Base case: if the pattern is exhausted, check if the string is also exhausted
+        if (j == p.size()) {
+            return memo[i][j] = (i == s.size());
         }
 
-        std::cout << "i : " << i << " - " << s.size() << std::endl;
-        std::cout << "j : " << j << " - " << p.size() << std::endl;
-        return i == s.size() && j == p.size();
+        // Check if the current character matches
+        bool firstMatch = (i < s.size() && (p[j] == s[i] || p[j] == '.'));
+
+        // If the next character is '*', we have two options:
+        if (j + 1 < p.size() && p[j + 1] == '*') {
+            // Case 1: Treat '*' as matching zero occurrences (skip current and next in pattern)
+            // Case 2: Treat '*' as matching one or more occurrences (if firstMatch is true)
+            memo[i][j] = (matchHelper(s, p, i, j + 2, memo) || (firstMatch && matchHelper(s, p, i + 1, j, memo)));
+        } else {
+            // No '*', so we must match the current character and move both pointers forward
+            memo[i][j] = firstMatch && matchHelper(s, p, i + 1, j + 1, memo);
+        }
+
+        return memo[i][j];
     }
 };
