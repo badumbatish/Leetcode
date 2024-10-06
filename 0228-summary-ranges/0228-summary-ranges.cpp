ac1cd45b5f52952a1bf808f12eb6616f854fa407
Jasmine Tang <tanghocle456@gmail.com>
2024-10-06 11:57:45 -0700

    Time: 0 ms (100%), Space: 8.5 MB (48.84%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        if (nums.empty()) return {};
+        std::vector<std::pair<int, int>> ranges;
+
+        int i = 0, j = 0;
+        while (i < nums.size() && j < nums.size()) {
+            if (j == 0 || nums[j] == nums[j-1] + 1) j++;
+            else {
+                std::cout << nums[i] << " " << nums[j-1] << std::endl;
+                ranges.push_back({nums[i], nums[j-1]});
+                i = j;
+                j = j + 1;
+            }
+        }
+
+        ranges.push_back({nums[i], nums[j-1]});
+
+        std::vector<std::string> result;
+        for (auto [start ,end ] : ranges) {
+            if (start != end) result.push_back(to_string(start) + "->" + to_string(end));
+            else result.push_back(to_string(start));
+        }
+
+        return result;
+    }
+};
