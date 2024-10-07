adb2ff00c3423920f11f77e3179cd5797d7af349
Jasmine Tang <tanghocle456@gmail.com>
2024-10-06 20:36:38 -0700

    Time: 205 ms (84.35%), Space: 9.8 MB (87.13%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,53 +1,40 @@
 class Solution {
+private:
+    using XY = std::pair<int, int>;
+    const std::vector<XY> directions = {{0, 1}, {-1, 0}, {1, 0}, {0, -1}};
+
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        // Run heuristic first
-            // Create a map of all character in board
-        std::unordered_map<char, int> board_count;
-        for(int8_t row=0; row < board.size(); row++) {
-            for (int8_t col=0; col < board[0].size(); col++) {
-                board_count[board[row][col]]++;
-            }
-        }
-            // Create a map of all characters in word
-        auto word_count = std::map<char, int>();
-        for (auto ch : word) word_count[ch]++;
-
-        for (auto it=word_count.begin(); it != word_count.end(); it++) {
-            // if the count of word is more than board, we know it's impossible
-            if (word_count[it->first] > board_count[it->first]) return false;
-        }
-        // make a marking array
-        for(int8_t row=0; row < board.size(); row++) {
-            for (int8_t col = 0; col < board[0].size(); col++) {
-                if (board[row][col] == word[0]) {
-                    if (helper(row, col, word, 0, board)) return true;
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
+                if (board[i][j] == word[0]) {
+                    char ch = board[i][j];
+                    board[i][j] = '.';
+                    if (find(board, word, i, j, 1)) {
+                        return true;
+                    }
+                    board[i][j] = ch;
                 }
             }
         }
         return false;
     }
 
-    bool helper(int8_t row, int8_t col, std::string& word,
-     int8_t index, vector<vector<char>>& board) {
-        if (index >= word.size()) return true;
+private:
+    bool find(vector<vector<char>>& board, const string& word, int i, int j, int index) {
+        if (index == word.length()) return true;
+        
+        for (auto [dx, dy] : directions) {
+            int x = i + dx, y = j + dy;
 
-        if (row < 0 || row == board.size() || 
-            col < 0 || col == board[0].size() || board[row][col] != word[index]) {
-                return false;
+            if (x >= 0 && x < board.size() && y >= 0 && y < board[0].size() && board[x][y] == word[index]) {
+                char ch = board[x][y];
+                board[x][y] = '.';
+                if (find(board, word, x, y, index + 1)) return true;
+                board[x][y] = ch;
             }
-
-        bool return_val = false;
-
-        board[row][col] = '#';
-        static std::vector<int> dir = {-1, 0, 1, 0 , -1};
-
-        for (int i = 0; i < 4; i++) {
-            return_val = helper(row + dir[i], col + dir[i+1], word, index+1, board);
-            if (return_val) break;
         }
 
-        board[row][col] = word[index];
-        return return_val;
+        return false;
     }
 };
