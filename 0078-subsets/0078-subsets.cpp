8a4be61d631967877f1d76fa321c6e891f81bac9
Jasmine Tang <tanghocle456@gmail.com>
2024-10-06 19:49:04 -0700

    Time: 0 ms (100%), Space: 8.7 MB (52.94%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,19 +1,32 @@
 class Solution {
-    std::vector<std::vector<int>> result;
-    std::vector<int> temp;
+    int n;
+    using Subset = std::vector<int>;
+    std::vector<Subset> result;
+private:
+    void print_subset(Subset& s) {
+        for (auto i : s) std::cout << i << " ";
+
+        std::cout << std::endl;
+    }
+    void backtrack(int index, std::vector<int>& nums, Subset& s) {
+        print_subset(s);
+        result.push_back(s);
+        if (index == n) return;
+
+        for (int i = index; i < n; i++) {
+            s.push_back(nums[i]);
+            backtrack(i+1, nums, s);
+            s.pop_back();
+        }
+    }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        result.push_back({});
-        dfs(nums, 0);
+       this->n = nums.size(); 
+
+        Subset s;
+        backtrack(0, nums, s);
+
         return result;
     }
 
-    void dfs(std::vector<int>&nums, int start) {
-        for (int i = start; i < nums.size(); i++) {
-            temp.push_back(nums[i]);
-            result.push_back(temp);
-            dfs(nums, i + 1);
-            temp.pop_back();
-        }
-    }
 };
