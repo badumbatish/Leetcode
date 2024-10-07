8740a6fa667947b8cfda1cd3928b43f1e74691af
Jasmine Tang <tanghocle456@gmail.com>
2024-10-07 02:50:22 -0700

    Time: 11 ms (20.1%), Space: 10.6 MB (81.55%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
@@ -5,13 +5,10 @@ public:
 
         for (int i = 0; i < s.size() ; i++) {
             stck.push_back(s[i]);
-            if (stck.size() < 2) continue;
 
-            while (stck.size() >= 2) {
-                auto sub = stck.substr(stck.size() - 2, 2);
-                if ( sub == "AB" || sub == "CD") {
-                    stck.pop_back(); stck.pop_back();
-                } else break;
+            while (stck.ends_with("AB") || stck.ends_with("CD"))  {
+                stck.pop_back(); stck.pop_back();
+
             }
         }
 
