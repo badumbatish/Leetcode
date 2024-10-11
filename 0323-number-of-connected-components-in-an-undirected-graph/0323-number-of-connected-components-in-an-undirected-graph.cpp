a219563d1b772c2da6fe76dcb42a66ed693941f1
Jasmine Tang <tanghocle456@gmail.com>
2024-10-11 06:01:10 -0700

    Time: 20 ms (33.2%), Space: 18.2 MB (32.88%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -1,30 +1,39 @@
 class Solution {
-    void dfs(std::vector<std::vector<int>> &adj_list, std::set<int>& visited, int source) {
-        visited.insert(source);
+    using Graph = std::vector<std::vector<int>>;
+    using Visited = std::vector<int>;
 
-        for (int i = 0; i < adj_list[source].size(); i++) {
-            if (visited.count(adj_list[source][i]) == 0) {
-                dfs(adj_list, visited, adj_list[source][i]);
+    Graph make_graph(int n, vector<vector<int>>& edges) {
+        Graph graph(n);
+        for (auto edge : edges) {
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+        }
+
+        return graph;
+    } 
+
+    void dfs(int i, Graph &graph, Visited &v) {
+        v[i] = 0;
+
+        for (auto neighbor : graph[i]) {
+            if (v[neighbor] == -1) {
+                dfs(neighbor, graph, v);
             }
         }
     }
 public:
-    int countComponents(int n, vector<vector<int>>& edges) {
-        std::vector<std::vector<int>> adj_list(n);
-        std::set<int> visited;
-        for(auto& edge : edges) {
-            adj_list[edge[1]].push_back(edge[0]);
-            adj_list[edge[0]].push_back(edge[1]);
-        }
 
-        int components = 0;
+    int countComponents(int n, vector<vector<int>>& edges) {
+        Graph graph = make_graph(n, edges);     
+        Visited visited(n, -1);
+        int connected_components = 0;
         for (int i = 0; i < n; i++) {
-            if (visited.count(i) == 0) {
-                components++;
-                dfs(adj_list, visited, i);
+            if (visited[i] == -1) {
+                dfs(i, graph, visited);
+                connected_components++;
             }
         }
 
-        return components;
+        return connected_components;
     }
 };
