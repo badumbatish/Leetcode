70ceef0e6a6dd60753c49a704c5b51dcff018702
Jasmine Tang <tanghocle456@gmail.com>
2024-10-11 16:36:00 -0700

    Time: 8 ms (52.66%), Space: 10.7 MB (21.89%) - LeetHub

--- 0422-valid-word-square/0422-valid-word-square.cpp
+++ 0422-valid-word-square/0422-valid-word-square.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     bool validWordSquare(vector<string>& words) {
         size_t expected_len = 0;
-        for (auto &word : words) {
-            expected_len = std::max(expected_len, word.size());
-        }
+        for (auto &word : words) expected_len = std::max(expected_len, word.size());
+        
 
-        for (auto &word : words) {
+        for (auto &word : words) 
             while (word.size() < expected_len) word+=" ";
-        }
-        while (words.size() < expected_len) 
-            words.push_back(std::string(' ', expected_len));
+        
+
+        std::string empty_string(' ',expected_len);
+        while (words.size() < expected_len) words.push_back(empty_string);
 
 
         for (int i = 0; i < words.size(); i++) {
