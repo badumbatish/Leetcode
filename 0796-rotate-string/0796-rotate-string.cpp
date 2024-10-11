ce0f8d3591e8cce0c3dd3142799fc391eb65d225
Jasmine Tang <tanghocle456@gmail.com>
2024-10-11 16:53:44 -0700

    Time: 3 ms (47.07%), Space: 7.4 MB (95%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,25 +1,7 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        if (s.size() != goal.size()) return false;
-        if (s.size() == 0) return false;
-
-
-        for (int i = 0; i < s.size(); i++) {
-            bool cont = true;
-            for (int j = 0; j < s.size(); j++) {
-                int index = (i + j) % s.size();
-                if (s[index] != goal[j]) {
-                    cont = false;
-                    break;
-                }
-            }
-
-            if (!cont) continue;
-            else return true; 
-        }
-
-
-        return false;
+        auto ss = s + s;
+        return s.size() == goal.size() && ss.find(goal) != std::string::npos;
     }
 };
