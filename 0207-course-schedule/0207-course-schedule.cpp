58f3314a9774b4aca7b93c4c48d6a4933a7c9b5b
Jasmine Tang <tanghocle456@gmail.com>
2024-10-13 15:38:03 -0700

    Time: 19 ms (50.07%), Space: 20.2 MB (14.81%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,35 +1,47 @@
+
 class Solution {
-public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses);
+    using Graph = std::vector<std::vector<int>>;
+
+    Graph make_graph(int n,  vector<vector<int>>& prerequisites) {
+        Graph adj(n);
 
         for(auto prerequisite : prerequisites) {
             adj[prerequisite[1]].push_back(prerequisite[0]);
         }
-        vector<bool> visit(numCourses);
-        vector<bool> inStack(numCourses);
 
-        for (int i = 0; i < numCourses; i++) {
-            if (dfs(i, adj, visit, inStack)) return false;
-        }
-        return true;
+        return adj;
+    }
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        Graph graph = make_graph(numCourses, prerequisites);
+        std::vector<int> indegree(numCourses, 0);
 
+        for (auto preq : prerequisites) {
+            indegree[preq[0]]++;
+        } 
+        std::queue<int> q;
+        for (int i = 0; i < numCourses; i++) 
+            if (indegree[i] == 0) q.push(i);
+
+        int nodesVisited = 0;
+
+        while (!q.empty()) {
+            int node = q.front(); q.pop();
+
+            nodesVisited++;
+
+
+            for (auto& neighbor : graph[node]) {
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) {
+                    q.push(neighbor);
+                }
+            }
+        } 
+
+        return nodesVisited == numCourses;
     }
 
-    bool dfs(int node, vector<vector<int>>&adj, vector<bool>&visit, vector<bool>& inStack) {
-        if (inStack[node]) return true;
-        if (visit[node]) return false;
-
-        visit[node] = true;
-        inStack[node] = true;
-
-        for (auto neighbor : adj[node]) {
-            if (dfs(neighbor, adj, visit, inStack)) return true;
-        }
-
-        inStack[node] = false;
-        return false;
-    }
 
 
 };
