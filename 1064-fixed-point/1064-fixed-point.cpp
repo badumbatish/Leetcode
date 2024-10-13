d0c116ce38545c7005eeb3688abd6e4acf2bd388
Jasmine Tang <tanghocle456@gmail.com>
2024-10-13 12:48:28 -0700

    Time: 3 ms (93.33%), Space: 13 MB (99.05%) - LeetHub

--- 1064-fixed-point/1064-fixed-point.cpp
+++ 1064-fixed-point/1064-fixed-point.cpp
@@ -9,14 +9,18 @@ public:
 
         int left = 0, right = arr.size() - 1;
 
+        int result = -1;
         while (left <= right) {
             int mid = (left + right) / 2;
 
-            if (arr[mid] == mid) return mid;
+            if (arr[mid] == mid)  {
+                result = mid;
+                right = mid -1;
+            }
             else if (arr[mid] < mid) left = mid + 1;
             else right = mid -1; 
         }
 
-        return -1;
+        return result;
     }
 };
