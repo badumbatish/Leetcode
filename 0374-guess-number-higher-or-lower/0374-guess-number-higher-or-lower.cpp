34a5a67de04bc8e549623f28c5a2848778012d2c
Jasmine Tang <tanghocle456@gmail.com>
2024-10-13 02:24:44 -0700

    Time: 0 ms (100%), Space: 7.6 MB (67.3%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
@@ -12,12 +12,16 @@ public:
     int guessNumber(int n) {
         int left = 0, right = n;
 
-        while (left < right) {
+        int number = -1;
+        while (left <= right) {
             int mid = left + (right - left) / 2;
             // If my guess is lower than the number being picked 
             // This means my guess needs to be higher 
-            if (guess(mid) == 1) left = mid + 1;
-            else right = mid;
+            if (guess(mid) == 0) {
+                return mid;
+            }  else if (guess(mid) == 1) 
+                left = mid + 1;
+             else right = mid - 1;
         }
         return left;
     }
