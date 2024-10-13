28e262817b01f65c205fe8d928513aa24df52bb6
Jasmine Tang <tanghocle456@gmail.com>
2024-10-13 16:15:55 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        int left = 1, right = *std::ranges::max_element(piles);
-
-        while (left < right) {
+        int64_t left = 1, right = *std::ranges::max_element(piles);
+        int64_t candidate = right;
+        while (left <= right) {
             int middle = left + (right - left) / 2;
             int hour_spent = 0;
 
             for (int pile : piles) hour_spent += pile / middle + (pile % middle != 0);
 
-            if (hour_spent <= h) right = middle;
+            if (hour_spent == h) {
+                right = middle - 1;
+                candidate = middle;
+
+
+            }  else if (hour_spent < h) {
+                right = middle - 1;
+                candidate = middle;
+            }
             else left = middle + 1;
         }
 
-        return right;
+        return candidate;
     }
 };
