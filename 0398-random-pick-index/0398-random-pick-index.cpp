dcd6442193a6c454199efe0310d8318abf134b04
Jasmine Tang <tanghocle456@gmail.com>
2024-10-13 20:19:35 -0700

    Time: 134 ms (49.75%), Space: 66.8 MB (25.9%) - LeetHub

---
+++ 0398-random-pick-index/0398-random-pick-index.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    std::map<int, std::vector<int>> mp;
+public:
+
+    Solution(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            // add in the group of target, the index i
+            mp[nums[i]].push_back(i);
+        }
+    }
+    
+    int pick(int target) {
+        // generate [0, 1] * len of our group
+        auto &group_target = mp[target];
+
+        double d = rand() % group_target.size(); 
+
+        return group_target[d];
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(nums);
+ * int param_1 = obj->pick(target);
+ */
