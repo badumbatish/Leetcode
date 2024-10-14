c25b38059cbd8a01c1d8070da7521044b6f005b2
Jasmine Tang <tanghocle456@gmail.com>
2024-10-13 19:51:56 -0700

    Time: 18 ms (45.09%), Space: 18 MB (17.64%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,43 +1,51 @@
 class Solution {
-public:
-    using Graph = std::unordered_map<int, std::vector<std::pair<int, int>>>;
+    using Graph = std::vector<std::vector<std::tuple<int, int>>>;
+    // 0 -> [....]  
+    // 1 -> [...........]  
+    // 2 -> [..]
 
-    Graph make_graph(vector<vector<int>>& flights)  {
-        Graph graph;
-        for (auto &flight : flights) {
+    Graph make_graph(int n, vector<vector<int>>& flights) {
+        Graph graph(n);
+
+        for (auto flight : flights) {
             graph[flight[0]].push_back({flight[1], flight[2]});
         }
+
         return graph;
     }
+    
+
+    // Current cost of flight - desitination - # of stops
+    using Data = std::tuple<int, int, int>;
+public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        // Make a graph adj list
-        Graph graph = make_graph(flights);
+        // Make graph
+        auto graph = make_graph(n, flights);
+        // Make pq (priority queue)
 
-        // BFS
+        //
+        std::priority_queue<Data, std::vector<Data>, greater<>> pq;
 
-        // Have a priority queue orignally src 
-        // Tuple of 3: cost, node, level 
-        using Data = std::tuple<int, int>;
-        std::queue<Data> pq; 
-        std::vector<int> visited(n, INT_MAX);
-        pq.push({0, src});
-        visited[src]= 0;
-        int stops = 0 ;
-        // Go until we found dst 
-        while (stops <= k  && !pq.empty()) {
-            int sz = pq.size();
-            while (sz--) {
-                auto [price, node] = pq.front(); pq.pop();
-                for (auto [to, distance] : graph[node] ) {
-                    if (price + distance >= visited[to]) continue;
-                    visited[to] = price+ distance;
-                    pq.push({visited[to], to});
-                }
-            }
-            stops++;
+        // Current cost is 0
+        // We were visiting src
+        // And we have used 0 stops
+        pq.push({0, src, 0});
+
+        std::vector<int> visited(n,  INT_MAX); 
+        // While pq .    
+        while (!pq.empty()) {
+            // top(): the least cost element in pq
+            auto [current_cost, node, num_stops] = pq.top(); pq.pop();
+
+            // The pq implicit invariant is that if we have visited it before, the cost will be lesser than what is current
+            if (num_stops > visited[node] || num_stops > k + 1) continue;
+
+            visited[node] = num_stops;
+
+            if (node == dst) return current_cost;
+            for (auto [neighbor, flight_cost] : graph[node]) pq.push({current_cost + flight_cost, neighbor, num_stops + 1});
         }
-        // return cheapest
 
-        return visited[dst] != INT_MAX ? visited[dst] : -1;
+        return -1;
     }
 };
