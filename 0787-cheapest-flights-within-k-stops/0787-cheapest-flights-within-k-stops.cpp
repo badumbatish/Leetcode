97e572e82498264b92e85af62f850ef6d5aae5b3
Jasmine Tang <tanghocle456@gmail.com>
2024-10-14 18:42:39 -0700

    Time: 692 ms (5%), Space: 53.2 MB (5.01%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -21,15 +21,15 @@ public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         std::vector<int> bellman(n, INT_MAX);
         bellman[src]  = 0;
+        auto temp = bellman;
         for (int i = 0; i <= k; i++) {
-            auto temp = bellman;
-            std::cout << "k is " << k << std::endl;
+            std::cout << "k is " << i << std::endl;
             for (auto flight : flights) {
                 if (bellman[flight[0]] == INT_MAX) continue;
                 temp[flight[1]] = std::min(temp[flight[1]], bellman[flight[0]] + flight[2]);
                 std::cout << flight[1] << " " << temp[flight[1]] << std::endl;
             }
-            bellman = temp;
+            std::swap(bellman, temp);
         }
         if (bellman[dst] == INT_MAX ){
             return -1;
