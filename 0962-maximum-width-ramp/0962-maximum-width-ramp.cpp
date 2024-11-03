753ec32db26c21f275f829e745ec128f9b2c4290
Jasmine Tang <tanghocle456@gmail.com>
2024-11-03 01:05:56 -0700

    Time: 11 ms (48.49%), Space: 47.7 MB (22.31%) - LeetHub

--- 0962-maximum-width-ramp/0962-maximum-width-ramp.cpp
+++ 0962-maximum-width-ramp/0962-maximum-width-ramp.cpp
@@ -2,25 +2,23 @@ class Solution {
 public:
     int maxWidthRamp(vector<int>& nums) {
         int n = nums.size();
-        vector<int> stack;  // Monotonic decreasing stack of indices
+        std::vector<int> stck;
 
-        // Step 1: Build a decreasing stack of indices
-        for (int i = 0; i < n; ++i) {
-            if (stack.empty() || nums[stack.back()] > nums[i]) {
-                stack.push_back(i);
+        for (int i = 0; i < n; i++) {
+            if (stck.empty() || nums[stck.back()] > nums[i]) {
+                stck.push_back(i);
             }
         }
 
-        int maxWidth = 0;
 
-        // Step 2: Traverse from right to left to find the maximum width ramp
-        for (int j = n - 1; j >= 0; --j) {
-            while (!stack.empty() && nums[stack.back()] <= nums[j]) {
-                maxWidth = max(maxWidth, j - stack.back());
-                stack.pop_back();
+        int max_width = 0;
+        for (int j =  n-1; j >=0; j--) {
+            while (!stck.empty() && nums[stck.back()] <= nums[j]) {
+                max_width = max(j - stck.back(), max_width);
+                stck.pop_back();
             }
         }
 
-        return maxWidth;
+        return max_width;
     }
 };
