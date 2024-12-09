6c7277b353dde7400c1a152010009aebaa76c8c6
Jasmine Tang <tanghocle456@gmail.com>
2024-12-09 14:11:54 -0800

    Time: 92 ms (20.26%), Space: 46.6 MB (55.04%) - LeetHub

--- 3152-special-array-ii/3152-special-array-ii.py
+++ 3152-special-array-ii/3152-special-array-ii.py
@@ -1,42 +1,12 @@
 class Solution:
     def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
-        ans = [False] * len(queries)
-        violating_indices = []
-        for i in range(1, len(nums)):
-            if nums[i] % 2 == nums[i-1] % 2: 
-                violating_indices.append(i)
         
-        for i in range(len(queries)):
-            start, end = queries[i]
-
-            found_violating_index = self.binarySearch(
-                start + 1, end, violating_indices
-            )
-
-            if found_violating_index:
-                ans[i] = False
-            else:
-                ans[i] = True
-
+        B = [i for i in range(1, len(nums)) if nums[i] % 2 == nums[i-1] % 2]
+        ans = []
+        for q in queries:
+            L = bisect_right(B, q[0])
+            R = bisect_right(B, q[1])
+            if R - L == 0: ans.append(True)
+            else: ans.append(False)
+        
         return ans
-
-    def binarySearch(
-        self, start: int, end: int, violating_indices: List[int]
-    ) -> bool:
-        left = 0
-        right = len(violating_indices) - 1
-        while left <= right:
-            mid = left + (right - left) // 2
-            violating_index = violating_indices[mid]
-
-            if violating_index < start:
-                # check right half
-                left = mid + 1
-            elif violating_index > end:
-                # check left half
-                right = mid - 1
-            else:
-                # violatingIndex falls in between start and end
-                return True
-
-        return False
