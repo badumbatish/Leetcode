05ffaeb547dbd1cef35dc00c59dc988919e2856c
Jasmine Tang <tanghocle456@gmail.com>
2025-01-11 02:04:01 -0800

    Time: 8 ms (21.85%), Space: 20 MB (10.61%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,26 +1,49 @@
 class Solution {
-public:
-    using Graph = std::unordered_map<int, std::vector<std::pair<int, int>>>;
 
-    Graph make_graph(vector<vector<int>>& flights)  {
-        Graph graph;
-        for (auto &flight : flights) {
+
+    using Cost = int;
+    using Dest = int;
+    using NumStop = int;
+    using Data = std::tuple<int, int, int>;
+    using Graph = std::vector<std::vector<std::tuple<Dest, Cost>>>;
+    // 0 -> [....]  
+    // 1 -> [...........]  
+    // 2 -> [..]
+
+    Graph make_graph(int n, vector<vector<int>>& flights) {
+        Graph graph(n);
+
+        for (auto flight : flights) {
             graph[flight[0]].push_back({flight[1], flight[2]});
         }
+
         return graph;
     }
+    
+
+    // Current cost of flight - desitination - # of stops
+public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        std::vector<int> dist(n, std::numeric_limits<int>::max() / 2);
+        auto graph = make_graph(n, flights);
+        std::priority_queue<Data, std::vector<Data>, greater<>> pq;
 
-        dist[src] = 0;
+        pq.push({0, src, 0});
 
-        std::vector<int> temp(dist);
-        for (int i = 0; i <= k; i++) {
-            for (auto &flight : flights) {
-                temp[flight[1]] = min(temp[flight[1]], dist[flight[0]] + flight[2]);
+        std::vector<int> visited(n,  INT_MAX); 
+        while (!pq.empty()) {
+            auto [current_cost, node, num_stops] = pq.top(); pq.pop();
+
+            if (num_stops > visited[node] || num_stops > k + 1) continue;
+
+            visited[node] = num_stops;
+
+            if (node == dst) return current_cost;
+            if (num_stops > k) continue;
+            for (auto [neighbor, flight_cost] : graph[node]) {
+                pq.push({current_cost + flight_cost, neighbor, num_stops + 1});
             }
-            std::swap(dist,temp);
         }
-        return dist[dst] == numeric_limits<int>::max() / 2 ? -1 : dist[dst];
+
+        return -1;
     }
 };
