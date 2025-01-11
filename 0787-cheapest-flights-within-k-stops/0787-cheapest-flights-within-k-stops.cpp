750cbc09c21ee0dd7a86cc0181aa4486a6bd4966
Jasmine Tang <tanghocle456@gmail.com>
2025-01-11 01:46:56 -0800

    Time: 16 ms (11.28%), Space: 16 MB (99.88%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,39 +1,26 @@
 class Solution {
-    using Graph = std::vector<std::vector<std::tuple<int, int>>>;
-    // 0 -> [....]  
-    // 1 -> [...........]  
-    // 2 -> [..]
+public:
+    using Graph = std::unordered_map<int, std::vector<std::pair<int, int>>>;
 
-    Graph make_graph(int n, vector<vector<int>>& flights) {
-        Graph graph(n);
-
-        for (auto flight : flights) {
+    Graph make_graph(vector<vector<int>>& flights)  {
+        Graph graph;
+        for (auto &flight : flights) {
             graph[flight[0]].push_back({flight[1], flight[2]});
         }
-
         return graph;
     }
-    
-
-    // Current cost of flight - desitination - # of stops
-    using Data = std::tuple<int, int, int>;
-public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        std::vector<int> bellman(n, INT_MAX);
-        bellman[src]  = 0;
-        auto temp = bellman;
+        std::vector<int> dist(n, std::numeric_limits<int>::max() / 2);
+
+        dist[src] = 0;
+
+        std::vector<int> temp(dist);
         for (int i = 0; i <= k; i++) {
-            std::cout << "k is " << i << std::endl;
-            for (auto& flight : flights) {
-                if (bellman[flight[0]] == INT_MAX) continue;
-                temp[flight[1]] = std::min(temp[flight[1]], bellman[flight[0]] + flight[2]);
-                std::cout << flight[1] << " " << temp[flight[1]] << std::endl;
+            for (auto &flight : flights) {
+                temp[flight[1]] = min(temp[flight[1]], dist[flight[0]] + flight[2]);
             }
-            std::swap(bellman, temp);
+            std::swap(dist,temp);
         }
-        if (bellman[dst] == INT_MAX ){
-            return -1;
-        } 
-        return bellman[dst];
+        return dist[dst] == numeric_limits<int>::max() / 2 ? -1 : dist[dst];
     }
 };
