2d9804c07b8acf4df72e77286b7fdd48e45240b9
Jasmine Tang <tanghocle456@gmail.com>
2025-01-10 16:22:11 -0800

    Time: 2 ms (77.79%), Space: 7.7 MB (69.58%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,12 +1,5 @@
 class Solution {
-    std::vector<int> dp = std::vector<int>(31, 0) ;
 public:
-    Solution() {
-        dp[0] = 0;
-        dp[1] = 1;
-        for (int i = 2; i <= 30; i++) 
-            dp[i] = dp[i-1] + dp[i-2];
-    }
     int fib(int n) {
         int first = 0, second = 1;
         if (n == 0) return 0;
