8fc7c79fb627f07e0193e16b2a8801fcbb816ab4
Jasmine Tang <tanghocle456@gmail.com>
2025-01-11 01:02:19 -0800

    Time: 56 ms (48.09%), Space: 51.2 MB (13.29%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int count = 0, sum = 0;
+
+        int count = 0, curr_sum = 0;
 
         std::unordered_map<int, int> mp = {{0, 1}};
 
         for (auto num : nums) {
-            sum += num;
-            count += mp[sum-k];
-            mp[sum]+=1;
+            curr_sum += num;
+            count += mp[curr_sum-k];
+            mp[curr_sum]++;
         }
 
         return count;
+    
     }
 };
