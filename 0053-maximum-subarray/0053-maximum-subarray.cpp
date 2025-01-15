54cf2a3b0c7ce1ada06b7e937003c18495542ec7
Jasmine Tang <tanghocle456@gmail.com>
2025-01-15 15:05:22 -0800

    Time: 0 ms (100%), Space: 71.6 MB (84.25%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int max_sum = 0;
-        int curr_sum = 0;
+        int curr_sum = nums.front();
+        int max_sum = nums.front();
 
-        for (int i = 0; i < nums.size(); i++) {
-            curr_sum = std::max(curr_sum + nums[i], nums[i]);
-
-            max_sum = std::max(max_sum, curr_sum);
+        for (int i = 1; i < nums.size(); i++) {
+            curr_sum = max(curr_sum + nums[i], nums[i]);
+            max_sum = max(curr_sum , max_sum);
         }
+
         return max_sum;
     }
 };
