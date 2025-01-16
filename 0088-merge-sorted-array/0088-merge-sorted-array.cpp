66373d9fa4c01856bd03ec1d668c64dc59a62412
Jasmine Tang <tanghocle456@gmail.com>
2025-01-15 21:33:49 -0800

    Time: 0 ms (100%), Space: 12.4 MB (33.21%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        std::vector<int> nums1_copy(nums1.begin(), nums1.begin() + m);
+        int p1 = m - 1, p2 = n - 1, p = m + n - 1;
 
-        int p1 = 0, p2 = 0;
-
-        int p = 0;
-        while (p1 < m && p2 < n) {
-            if (nums1_copy[p1] < nums2[p2]) nums1[p++] = nums1_copy[p1++];
-            else nums1[p++] = nums2[p2++];
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] > nums2[p2]) nums1[p--] = nums1[p1--];
+            else nums1[p--] = nums2[p2--];
         }
-
-        while (p1 < m) nums1[p++] = nums1_copy[p1++];
-        while (p2 < n) nums1[p++] = nums2[p2++];
+        while (p1 >= 0) nums1[p--] = nums1[p1--];
+        while (p2 >= 0) nums1[p--] = nums2[p2--];
     }
 };
