1393b8635e11435f641e0af3e1fa687ab2a2083c
Jasmine Tang <tanghocle456@gmail.com>
2025-01-15 19:46:42 -0800

    Time: 649 ms (5.08%), Space: 247.3 MB (5%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,24 +1,19 @@
+#include <ranges>
+using namespace ranges;
+using namespace views;
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-
-        std::unordered_map<char, int> freq_map;
-
-        int left = 0, right = 0;
-        int max_len = 0;
-        while (right < s.size()) {
-            char ch_r = s[right];
-            freq_map[ch_r]++;
-
-            while (freq_map[ch_r] > 1) {
-                char ch_l = s[left];
-                freq_map[ch_l]--;
-                left++;
+        size_t max_length = 0;
+        for (auto [i, ch1] : enumerate(s)) {
+            std::unordered_set<char> char_set;
+            for (auto [j, ch2] : enumerate(s) | drop(i)) {
+                char_set.insert(ch2);
+                if (char_set.size() != (j-i+1)) break;
+                max_length = std::max(max_length, (size_t)j-i+1);
             }
-            max_len = max(max_len, right-left+1);
-            right++; 
         }
 
-        return max_len;
+        return max_length;
     }
 };
