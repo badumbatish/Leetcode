24e9b24671dfb2347f70e43177038cd033bcae7c
Jasmine Tang <tanghocle456@gmail.com>
2025-01-16 19:37:29 -0800

    Time: 7 ms (43.89%), Space: 24.8 MB (14.85%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,20 +1,18 @@
+#include <ranges>
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        std::ranges::sort(intervals, [](auto &a, auto &b) { return a[0] < b[0]; });
-
-        std::vector<int> temp = intervals[0];
-
-        std::vector<std::vector<int>> ans = {temp};
-        
-        for (auto &interval : intervals) {
-            auto& last_interval = ans.back();
-            if (interval[0] > last_interval[1] )  {
-                ans.push_back(interval);
+        std::ranges::sort(intervals);
+        vector<vector<int>> result; 
+        result.push_back(intervals.front());
+        for (auto interval : intervals) {
+            // if doesn't clash
+            if (result.back()[1] < interval[0]) {
+                result.push_back(interval);
             } else {
-                last_interval[1] = std::max(last_interval[1], interval[1]);
+                result.back()[1] = std::max(result.back()[1], interval[1]);
             }
         }
-        return ans;
+        return result;
     }
 };
