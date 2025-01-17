4592ce40bfd945017d535e5e67e69cae1f4b2b0c
Jasmine Tang <tanghocle456@gmail.com>
2025-01-16 20:49:07 -0800

    Time: 68 ms (11.49%), Space: 19.8 MB (39.6%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,34 +1,64 @@
+#include <ranges>
+class UF {
+    std::vector<int> v;
+    std::vector<int> rank;
+    int count = 0;
+    public:
+    UF(std::vector<std::vector<char>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                }
+                v.push_back(i * n + j);
+                rank.push_back(0);
+            }
+        }
+        assert(v.size() == grid.size() * grid[0].size());
+    }
+    int find(int i) {
+        if (v[i] != i) v[i] = find(v[i]);
+        return v[i];
+    }
+    void connect(int a, int b) {
+        auto fa = find(a), fb = find(b);
+        if (fa == fb) return;
+        if (rank[fa] > rank[fb])
+            v[fb] = fa;
+        else {
+            v[fa] = fb;
+            if (rank[fa] == rank[fb]) rank[fb] = rank[fb] + 1;
+        }
+        count--;
+        std::cout << count << std::endl;
+    }
+    int size() {
+        return count;
+    }
+};
 class Solution {
-    bool visited[301][301] = { false};
-    int rows = 0;
-    int cols = 0;
-    int num_island = 0;
 public:
     int numIslands(vector<vector<char>>& grid) {
-        rows = grid.size();
-        cols = grid[0].size();
+        static std::array<int, 5> dir = {-1, 0, 1, 0, -1};
+        auto is_within = [&](auto a, auto b) { return 0 <= a && a < grid.size() && 0 <= b && b < grid[0].size(); };
+        UF uf(grid);
+int m = grid.size(), n = grid[0].size();
 
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[i].size(); j++) {
-                if (grid[i][j] == '1') {
-                    dfs(grid, i , j);
-                    num_island++;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {  // Only process land cells
+                    for (int d = 0; d < 4; d++) {
+                        int x = i + dir[d], y = j + dir[d + 1];
+                        if (is_within(x, y) && grid[x][y] == '1') {
+                            uf.connect(i * n + j, x * n + y);  // FIXED: Correct indexing
+                        }
+                    }
                 }
             }
-        }     
-        return num_island;
-    }
-
-    void dfs(std::vector<std::vector<char>>&grid, int row, int col) {
-        grid[row][col] = '0';
-        static std::vector<std::tuple<int, int>> dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-
-        for (auto [row_dir, col_dir] : dirs) {
-            int new_row = row + row_dir, new_col = col + col_dir;
-            if (!(0 <= new_row && new_row < rows)) continue;
-            if (!(0 <= new_col && new_col < cols)) continue;
-
-            if (grid[new_row][new_col] == '1') dfs(grid, new_row, new_col);
         }
+        return uf.size();
     }
 };
