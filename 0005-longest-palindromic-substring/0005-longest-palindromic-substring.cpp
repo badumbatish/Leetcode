448829f2f43504a1bbea7a871cc27562f2ae7fc1
Jasmine Tang <tanghocle456@gmail.com>
2025-01-17 18:30:18 -0800

    Time: 7 ms (89.01%), Space: 9.4 MB (83.87%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,27 +1,19 @@
 class Solution {
-public:
     std::string_view expand(int i, int j, std::string_view s) {
-        // Starting from i and j, we go out both directions 
-        // until either s[i] != s[j]
-        // or they went out of the bound of the string view
-
-        while (i >= 0 && j < s.size() && s[i] == s[j]) {
-            i--;
-            j++;
+        int left = i, right = j;
+        while (left >= 0 && right < s.size() && s[left] == s[right]) {
+            left--; right++;
         }
-
-        return s.substr(i + 1, j - i - 1);
+        return s.substr(left + 1, right - left - 1);
     }
+public:
     string longestPalindrome(string s) {
         std::string_view ans = "";
-
-        for (int i = 0 ; i < s.size(); i++) {
-            decltype(ans) odd = expand(i, i , s);
+        for (int i = 0; i < s.size(); i++) {
+            auto odd = expand(i, i, s);
             if (odd.size() > ans.size()) ans = odd;
-
-            decltype(ans) even = expand(i, i + 1, s);
+            auto even = expand(i, i+1, s);
             if (even.size() > ans.size()) ans = even;
-
         }
         return std::string(ans);
     }
