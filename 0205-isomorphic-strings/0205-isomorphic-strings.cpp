dd5e3c1a84f7dbfd401b661a4343037b5eeed1ba
Jasmine Tang <tanghocle456@gmail.com>
2025-01-17 21:22:28 -0800

    Time: 7 ms (9.4%), Space: 10.9 MB (8.41%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,17 +1,17 @@
 #include <ranges>
 class Solution {
+    std::vector<int> transformed(const std::string & str) {
+        std::unordered_map<char, int> i_map;
+        std::vector<int> v;
+
+        for (auto [i, ch] : std::ranges::views::enumerate(str)) {
+            if (!i_map.contains(ch)) i_map[ch] = i;
+            v.push_back(i_map[ch]);
+        }
+        return v;
+    }
 public:
     bool isIsomorphic(string s, string t) {
-        std::unordered_map<char, char> m1, m2;
-        for (auto [ch1, ch2] : std::ranges::views::zip(s, t)) {
-            if (!m1.contains(ch1))
-                m1[ch1] = ch2;
-            if (m1[ch1] != ch2) return false;
-
-            if (!m2.contains(ch2))
-                m2[ch2] = ch1;
-            if (m2[ch2] != ch1) return false;
-        }
-        return true;
+        return transformed(s) == transformed(t);
     }
 };
