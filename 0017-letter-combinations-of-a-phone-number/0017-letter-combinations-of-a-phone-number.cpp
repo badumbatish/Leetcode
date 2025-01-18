129c93a377217ecd9b98d5a15daed645e71d3fcd
Jasmine Tang <tanghocle456@gmail.com>
2025-01-17 18:37:43 -0800

    Time: 0 ms (100%), Space: 9.4 MB (34.98%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,38 +1,31 @@
 class Solution {
-    std::map<int, std::string> digits_map = {
-        {1, ""},
-        {2, "abc"},
-        {3, "def"},
-        {4, "ghi"},
-        {5, "jkl"},
-        {6, "mno"},
-        {7, "pqrs"},
-        {8, "tuv"},
-        {9, "wxyz"}
+    std::unordered_map<char, std::string> mp = {
+        {'2', {"abc"}},
+        {'3', {"def"}},
+        {'4', {"ghi"}},
+        {'5', {"jkl"}},
+        {'6', {"mno"}},
+        {'7', {"pqrs"}},
+        {'8', {"tuv"}},
+        {'9', {"wxyz"}},
     };
 
-    int length;
-    std::vector<std::string> result;
     std::string temp;
+    std::vector<std::string> result;
+    void dfs(int i, const std::string& digits) {
+        if (temp.size() == digits.size()) result.push_back(temp);
+        else {
+            for (auto ch : mp[digits[i]]) {
+                temp.push_back(ch);
+                dfs(i + 1, digits);
+                temp.pop_back();
+            }
+        }
+    }
 public:
     vector<string> letterCombinations(string digits) {
-        if (digits.size() == 0) return {};
-        length = digits.size();
-        dfs(digits, 0);
-
+        if (digits.size() != 0) 
+            dfs(0, digits);
         return result;
-    }
-
-    void dfs(std::string& digits, int start) {
-        if (temp.size() == length) {
-            result.push_back(temp);
-            return;
-        }
-
-        for (int i = 0; i < digits_map[digits[start] - '0'].size(); i++) {
-            temp.push_back(digits_map[digits[start] - '0'][i]);
-            dfs(digits, start + 1);
-            temp.pop_back();
-        }
     }
 };
