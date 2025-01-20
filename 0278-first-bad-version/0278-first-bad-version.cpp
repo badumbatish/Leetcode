3876fa3d8eb56fbb1f4acc5fb35efbf71c64c17a
Jasmine Tang <tanghocle456@gmail.com>
2025-01-20 10:59:49 -0800

    Time: 2 ms (54.1%), Space: 7.9 MB (34.58%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -6,7 +6,7 @@ public:
     int firstBadVersion(int n) {
         int lo = 0, hi = n;
         while (lo + 1 < hi) {
-            auto mid = lo + (hi - lo)/2;
+            int mid = lo + (hi - lo)/2;
             if (isBadVersion(mid)) hi = mid;
             else lo = mid;
         }
