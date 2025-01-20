1ed4ac1566f6123d019a3dd14d03210ec4bc3848
Jasmine Tang <tanghocle456@gmail.com>
2025-01-20 11:11:07 -0800

    Time: 0 ms (100%), Space: 9.1 MB (73.29%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        std::unordered_map<char, int> mp;
-        for (auto ch : s) mp[ch]++;
-        for (auto ch : t) {
-            if (!mp.contains(ch)) return ch;
-            mp[ch]--;
-            if (mp[ch] == 0) mp.erase(ch);
-        }
-        return 'x';
+        char i = 0;
+        for (auto ch : s) i^=ch;
+        for (auto ch : t) i^=ch;
+        return i;
     }
 };
