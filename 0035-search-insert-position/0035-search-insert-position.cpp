27b5da94d110b7f6970f14f06d30eaefd7b2b7c6
Jasmine Tang <tanghocle456@gmail.com>
2025-01-21 01:17:41 -0800

    Time: 0 ms (100%), Space: 13.7 MB (36.99%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        // Lower bound of a number in nums means the first occurrence which is not less than num
-        int left = 0, right = nums.size();
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] < target) {
-                left = mid + 1;
-            } else right = mid;
-        }
+        int left = -1, right = nums.size() ;
 
-        return left;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= target) right = mid;
+            else left = mid;
+        }
+        return right;
     }
 };
