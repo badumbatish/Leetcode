1a9aae1a17cbc7785855e13b75d09a3068e03fd7
Jasmine Tang <tanghocle456@gmail.com>
2025-01-20 20:56:33 -0800

    Time: 1995 ms (20.82%), Space: 51.2 MB (14.06%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
+        // [0][1, 2, -3, 3]
+        std::unordered_map<int, int> mp = { {0, 1}}; // stores the freq of all subarray starting from 0 that adds up to a sum
+        //  
+        int prefix_sum = 0, count = 0; 
 
-        int count = 0, curr_sum = 0;
+        // k : target
+        // s : prev sum
+        // n : curr num
 
-        std::unordered_map<int, int> mp = {{0, 1}};
+        // k = s + 
 
+        // 3 4 -7 7 k = 0
+        // 0,
+        int i = 0;
         for (auto num : nums) {
-            curr_sum += num;
-            count += mp[curr_sum-k];
-            mp[curr_sum]++;
+            std::cout << "At i : " << i++ << "\n";
+            prefix_sum += num;
+            std::cout << "prefix_sum is: " << prefix_sum << std::endl;
+            std::cout << "The count of ... " << mp[prefix_sum-k] << std::endl;
+            count += mp[prefix_sum - k];
+            mp[prefix_sum]++;
         }
-
         return count;
-    
     }
 };
