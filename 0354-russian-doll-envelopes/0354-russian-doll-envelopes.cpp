b37d81d60939c97b8080fa036a4fa63682f3dff9
Jasmine Tang <tanghocle456@gmail.com>
2025-01-25 21:57:52 -0800

    Time: 51 ms (37.45%), Space: 81.7 MB (61.01%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -1,30 +1,24 @@
 class Solution {
-    
 public:
-    int maxEnvelopes(vector<vector<int>>& envelopes) {
-        std::ranges::sort(envelopes, [](auto &a, auto &b) {
-            if (a[0] < b[0]) return true;
-            else if (a[0] == b[0]) return a[1] > b[1];
-            else return false;
-        });
-        int n = envelopes.size();
-        std::vector<std::vector<int>> sub;
+    static bool comp(vector<int> &a, vector<int> &b){
+        if(a[0]==b[0]){
+            return a[1]>b[1];
+        }
+        return a[0]<b[0];
+    }
         
-        sub.push_back(envelopes.front());
-        auto less = [](auto& a, auto &b) {
-            return a[0] < b[0] && a[1] < b[1];
-        };
-
-        for (int i = 1; i < n; i++) {
-            auto doll = envelopes[i];
-            if (less(sub.back(), doll)) {
-                sub.push_back(doll);
-            } else {
-                int j = 0;
-                while (less(sub[j] , doll)) j++;
-                sub[j] = doll;
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        sort(envelopes.begin(), envelopes.end(), comp);
+        int i, j, n=envelopes.size();
+        vector<int>lis;
+        
+        for(i=0; i<n; i++){
+            auto it = lower_bound(lis.begin(), lis.end(), envelopes[i][1]);
+            if(it==lis.end()){lis.push_back(envelopes[i][1]);}
+            else{
+                *it = envelopes[i][1];
             }
         }
-        return sub.size();
+        return lis.size();
     }
 };
