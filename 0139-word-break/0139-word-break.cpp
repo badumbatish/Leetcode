00d486ab9c2295c2dfc75d1e4a4edda75ac5a4ae
Jasmine Tang <tanghocle456@gmail.com>
2025-01-25 16:54:34 -0800

    Time: 11 ms (47.55%), Space: 16.3 MB (34.06%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        std::unordered_set<std::string> words(wordDict.begin(), wordDict.end());
+
+        std::queue<int> queue;
+        std::vector<bool>seen(s.length(), false);
+
+        queue.push(0);
+
+        while (!queue.empty()) {
+            int start = queue.front(); queue.pop();
+            if (start == s.length()) return true;
+
+            for (int end = start + 1;  end <= s.length(); end++) {
+                if (seen[end]) continue;
+                if (words.find(s.substr(start, end - start)) != words.end()) {
+                    queue.push(end);
+                    seen[end] = true;
+                }
+            }
+        }
+        return false;
+    }
+};
