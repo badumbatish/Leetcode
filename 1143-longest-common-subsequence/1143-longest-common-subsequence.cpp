517c37604bebf3855999026304645d73b0b2a59b
Jasmine Tang <tanghocle456@gmail.com>
2025-01-25 19:10:20 -0800

    Time: 17 ms (85.69%), Space: 10.6 MB (87.41%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -2,9 +2,22 @@ class Solution {
 public:
 int longestCommonSubsequence(string &a, string &b) {
     short m[1001][1001] = {};
-    for (auto i = 0; i < a.size(); ++i)
-        for (auto j = 0; j < b.size(); ++j)
-            m[i + 1][j + 1] = a[i] == b[j] ? m[i][j] + 1 : max(m[i + 1][j], m[i][j + 1]);
+    for (auto i = 1; i <= a.size(); ++i)
+        for (auto j = 1; j <= b.size(); ++j)
+        // if current one are equal, then we accept both char at each string, this forms the base for the next chars
+        // else we need to consider 
+            m[i][j] = a[i-1] == b[j-1] ? m[i-1][j-1] + 1 : max(m[i][j-1], m[i-1][j]);
     return m[a.size()][b.size()];
+
+    /*
+          012345
+        0|000
+        1| 1
+        2
+        3
+        4
+
+
+    */
 }
 };
