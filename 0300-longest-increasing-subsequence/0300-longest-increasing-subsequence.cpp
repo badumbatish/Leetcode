7a307492b8322623be6aef5d5f2ec8ce2e8877a2
Jasmine Tang <tanghocle456@gmail.com>
2025-01-25 21:00:27 -0800

    Time: 0 ms (100%), Space: 13.9 MB (94.52%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        std::vector<int> dp(nums.size(), 1);
+        std::vector<int> sub;
+        sub.push_back(nums.front());
 
-        // In the DPV textbook, we define (i, j) to have an edge if nums[j] < nums[i]
         for (int i = 1; i < nums.size(); i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i])
-                    dp[i] = std::max(dp[i], dp[j] + 1);
+            int num = nums[i];
+            if (num > sub.back()) sub.push_back(num);
+            else {
+                int j = 0;
+                while (num > sub[j]) j++;
+                sub[j] = num;
             }
         }
 
-        return *std::max_element(dp.begin(), dp.end());
+        return sub.size();
+        
     } 
 };
