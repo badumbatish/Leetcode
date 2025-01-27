c8f0697c8c64c46c477630d283bc79fde3fa52db
Jasmine Tang <tanghocle456@gmail.com>
2025-01-26 20:52:00 -0800

    Time: 0 ms (100%), Space: 11.8 MB (74.1%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -5,27 +5,20 @@ public:
         std::vector<std::vector<int>> unique_paths(m, std::vector<int>(n, 0));
 
         int i = 0;
-        while (i < m) {
+        bool no_obstacle = true;
+        // first column's row
+        for (int i = 0; i < m; i++) {
             // Have an obstacle, anything after this is not cool
-            if (obstacleGrid[i][0] == 1) {
-                for (; i < m; i++) unique_paths[i][0] = 0;
-
-            } else {
-                unique_paths[i][0] = 1;
-                i++;
-            }
+            no_obstacle &= obstacleGrid[i][0] == 0;
+            unique_paths[i][0] = no_obstacle;
         }
 
-        i = 0;
-        while (i < n) {
+        // first row's columns
+        no_obstacle = true;
+        for (int i = 0; i < n; i++) {
             // Have an obstacle, anything after this is not cool
-            if (obstacleGrid[0][i] == 1) {
-                for (; i < n; i++) unique_paths[0][i] = 0;
-
-            } else {
-                unique_paths[0][i] = 1;
-                i++;
-            }
+            no_obstacle &= obstacleGrid[0][i] == 0;
+            unique_paths[0][i] = no_obstacle;
         }
 
         for (int i = 1; i < m; i++) {
