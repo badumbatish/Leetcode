df201f75c58f52eb58e1f02d3603e63dd5526d42
Jasmine Tang <tanghocle456@gmail.com>
2025-01-29 21:11:23 -0800

    Time: 22 ms (68.89%), Space: 59.9 MB (82.95%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.cpp
+++ 0706-design-hashmap/0706-design-hashmap.cpp
@@ -1,24 +1,30 @@
+#include <ranges>
 class MyHashMap {
     // 1. HashMap is between 1-10000 (some small number) -> array
     // 2. An array of N buckets, where each buckets is some size M -> std::vector<std::list<int>>;
     //      hash an integer -> 0 to N -> index into it, search add to list if not exist.
-    std::vector<int> arr = std::vector(1000001, -1);
+    std::vector<std::list<std::pair<int, int>>> arr = std::vector<std::list<std::pair<int,int>>>(20);
 public:
     MyHashMap() {
         
     }
     
     void put(int key, int value) {
-        arr[key] = value;
+        auto search = std::ranges::find_if(arr[key % 20], [&](auto x) { return x.first == key;});
+        if (search == arr[key % 20].end()) 
+            arr[key%20].push_back({key, value});
+        else search->second = value;
     }
     
     int get(int key) {
-        return arr[key];
-        
+        for (auto [k, value] : arr[key % 20]) {
+            if (k == key) return value;
+        }
+        return -1;
     }
     
     void remove(int key) {
-       arr[key] = -1;
+        arr[key % 20].remove_if([&](auto p) {return p.first == key;});
     }
 };
 
