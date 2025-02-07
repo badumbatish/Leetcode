eb2062a5d00eb006411f90c9d5e4945d26d841bd
Jasmine Tang <tanghocle456@gmail.com>
2025-02-06 20:23:05 -0800

    Time: 291 ms (10.94%), Space: 174.8 MB (11.25%) - LeetHub

---
+++ 3160-find-the-number-of-distinct-colors-among-the-balls/3160-find-the-number-of-distinct-colors-among-the-balls.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> queryResults(int limit, vector<vector<int>>& queries) {
+        std::set<int> distinct_color;
+        std::map<int, int> color_directory, color_count;
+
+        std::vector<int> query_answer;
+        for (auto q : queries) {
+            auto ball = q[0], color = q[1];
+            if (!color_directory.contains(ball)) {
+                color_directory[ball] = color;
+                color_count[color]++;
+            } else {
+                auto ball_color = color_directory[ball];
+                if (ball_color != color) {
+                    color_count[ball_color]--;
+                    if (color_count[ball_color] == 0) color_count.erase(ball_color);
+                    color_count[color]++;
+                } 
+                color_directory[ball] = color;
+            }
+
+
+            query_answer.push_back(color_count.size());
+        }    
+        return query_answer;
+    }
+};
